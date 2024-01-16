dcb53bde15b279298cc91270dc1e5d3590f9954d
Firas Rebai <Rebaifiras864@gmail.com>
2024-01-16 23:48:31 +0100

    the end

---
+++ app/(drawer)/(tabs)/(index)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from 'expo-router'
+import { View, Text } from 'react-native'
+
+
+
+export default function FeedLayout() {
+  return (
+    <Stack screenOptions={{headerShown: false}}/>
+  )
+}
